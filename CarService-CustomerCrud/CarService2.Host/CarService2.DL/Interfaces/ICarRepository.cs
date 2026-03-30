d0999e34561c4b3829d6da59f7b4095255d617a7
Hoop-03 <pliomba.jr45@gmail.com>
2026-03-30 18:12:20 +0300

    Adds CustomerCrud

---
+++ CarService-CustomerCrud/CarService2.Host/CarService2.DL/Interfaces/ICarRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarService3.Models.Entities;
+
+namespace CarService3.DL.Interfaces
+{
+    public interface ICarRepository
+    {
+        void Add(Car? car);
+        List<Car> GetAll();
+        Car? GetById(Guid id);
+        void Delete(Guid id);
+    }
+}
